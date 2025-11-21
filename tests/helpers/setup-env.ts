0645b585750f3e1ad76a72bd7f4d4e1ea2c0e07b
Anisur Rahman <77926360+shield75@users.noreply.github.com>
2025-11-21 21:38:34 +0600

    Global config setup

--- tests/helpers/setup-env.ts
+++ tests/helpers/setup-env.ts
@@ -3,51 +3,60 @@ import fs from "fs";
 import path from "path";
 import type { FullConfig } from "@playwright/test";
 
+
 const globalSetup = async (config: FullConfig) => {
-    const resultsDir = path.join(process.cwd(), "allure-results");
-    if (!fs.existsSync(resultsDir)) fs.mkdirSync(resultsDir, { recursive: true });
 
-    // Environment
-    const envData = [
-        `Browser=Chromium`,
-        `OS=${process.platform}`,
-        `Build=1.0.0`,
-        `App=Swag Labs`,
-        `Tester=Anisur Rahman`,
-        `Environment=Staging`
-    ].join("\n");
-    fs.writeFileSync(path.join(resultsDir, "environment.properties"), envData, "utf-8");
+    console.log(">> Global setup started...");
 
-    // Categories
-    const categoriesSrc = path.join(process.cwd(), "data/allure/allure-categories.json");
-    const categoriesDest = path.join(resultsDir, "categories.json");
-    fs.copyFileSync(categoriesSrc, categoriesDest);
+    if (process.env.ENV?.toUpperCase() == "LOCAL") {
+        console.log(">> Setting up for LOCAL environment");
 
-    // Executor (manual values that ALWAYS show)
-    const executor = {
-        name: "Anisur Rahman",
-        type: "local",
-        build: "1.0.0",
-        url: "https://www.saucedemo.com/",
-        reportUrl: "https://github.com/shield75/playwright-e2e-test.git",
-        description: "Tests executed locally by Anisur Rahman",
-        stage: "Staging",
+        const resultsDir = path.join(process.cwd(), "allure-results");
+        if (!fs.existsSync(resultsDir))
+            fs.mkdirSync(resultsDir, { recursive: true });
 
-        parameters: [
-            { name: "OS", value: `${process.platform} ${process.arch}` },
-            { name: "Agent", value: `Node.js ${process.version}` },
-            { name: "Environment", value: "Staging" },
-            { name: "App", value: "Swag Labs" }
-        ]
+        // Environment
+        const envData = [
+            `Browser=Chromium`,
+            `OS=${process.platform}`,
+            `Build=1.0.0`,
+            `App=Swag Labs`,
+            `Tester=Anisur Rahman`,
+            `Environment=Staging`
+        ].join("\n");
+        fs.writeFileSync(path.join(resultsDir, "environment.properties"), envData, "utf-8");
+
+        // Categories
+        const categoriesSrc = path.join(process.cwd(), "data/allure/allure-categories.json");
+        const categoriesDest = path.join(resultsDir, "categories.json");
+        fs.copyFileSync(categoriesSrc, categoriesDest);
+
+        // Executor (manual values that ALWAYS show)
+        const executor = {
+            name: "Anisur Rahman",
+            type: "local",
+            build: "1.0.0",
+            url: "https://www.saucedemo.com/",
+            reportUrl: "https://github.com/shield75/playwright-e2e-test.git",
+            description: "Tests executed locally by Anisur Rahman",
+            stage: "Staging",
+
+            parameters: [
+                { name: "OS", value: `${process.platform} ${process.arch}` },
+                { name: "Agent", value: `Node.js ${process.version}` },
+                { name: "Environment", value: "Staging" },
+                { name: "App", value: "Swag Labs" }
+            ]
+        };
+
+        fs.writeFileSync(
+            path.join(resultsDir, "executor.json"),
+            JSON.stringify(executor, null, 2),
+            "utf-8"
+        );
+
+        console.log("✅ Allure environment, categories, and executor are ready for local environment.");
     };
-
-    fs.writeFileSync(
-        path.join(resultsDir, "executor.json"),
-        JSON.stringify(executor, null, 2),
-        "utf-8"
-    );
-
-    console.log("✅ Allure environment, categories, and executor are ready.");
-};
+}
 
 export default globalSetup;
