c981283b1ba99ba25dc406b5d69638569f295372
Anisur Rahman <77926360+shield75@users.noreply.github.com>
2025-12-06 01:14:26 +0600

    helpers init

--- tests/helpers/attach-screenshot.ts
+++ tests/helpers/attach-screenshot.ts
@@ -1,8 +1,30 @@
-import { Page, TestInfo } from '@playwright/test';
+import { test, Page, TestInfo, Locator } from '@playwright/test';
 
 export async function attachScreenshot(name: string, page: Page, testInfo: TestInfo) {
     await testInfo.attach(name, {
         body: await page.screenshot({ fullPage: true }),
         contentType: 'image/png'
+    });
+}
+
+/**Ful page Screenshot */
+async function takeFullPageScreenshot(page: Page, screenshotName: string) {
+    const screenshot = await page.screenshot({ fullPage: true });
+    // Attach it to the report
+    await test.info().attach(screenshotName, {
+        body: screenshot,
+        contentType: "image/png",
+    });
+}
+
+/**Element Screenshot */
+async function takeElementScreenshot(element: Locator, screenshotName: string) {
+    // Take screenshot of the element
+    const screenshot = await element.screenshot();
+
+    // Attach it to the report
+    await test.info().attach(screenshotName, {
+        body: screenshot,
+        contentType: "image/png",
     });
 }
--- tests/helpers/file-helper.ts
+++ tests/helpers/file-helper.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import { log } from "./logger.js";
+import path from "path";
 /**
  * Reads file and returns string. For JSON, parse it before using
  */
@@ -18,10 +19,17 @@ function readFile(filePath: string): any {
  */
 function writeFile(filePath: string, data: string) {
     try {
+        // Ensure parent directory exists
+        const dir = path.dirname(filePath);
+
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+        }
+
         fs.writeFileSync(filePath, data);
         log("info", `Writing file: ${filePath}...`);
     } catch (err) {
-        new Error(`Error writing to: ${filePath}, ${err}`);
+        throw new Error(`Error writing to: ${filePath}, ${err}`);
     }
 }
 
